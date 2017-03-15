ae9a6b8fc65c02661f263a840505e86cb148a4a1
Kevin Nolan <betaalpha@yahoo.com>
2017-03-15 12:44:45 +0000

    Added pizza pomodoro

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {}); /*Page needs no data passed to it*/
 });
 
 module.exports = router;
